b97deee46e72f8827dc219cda68a7e63a7d48b8b
amiraliikh83 <amiraliikh83@gmail.com>
2024-05-27 18:00:50 +0330

    BedeBere

--- src/ForgetPassword.jsx
+++ src/ForgetPassword.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
@@ -92,16 +88,21 @@ const ForgetPassword = () => {
               <Button
                 type="submit"
                 fullWidth
-                style={{width:"54vh", display : "flex" , alignItems : "center"}}
+                style={{ width: "54vh", display: "flex", alignItems: "center" }}
                 size="large"
                 variant="contained"
-                sx={{ mt: 3, mb:3 }}
+                sx={{ mt: 3, mb: 3 }}
               >
                 ارسال لینک بازیابی
               </Button>
               <Grid container justifyContent="center">
                 <Grid item>
-                  <Link href="/login" variant="h6" fontStyle={"unset"} style={{}}>
+                  <Link
+                    href="/login"
+                    variant="h6"
+                    fontStyle={"unset"}
+                    style={{}}
+                  >
                     . قبلا ثبت نام کرده اید؟ وارد شوید
                   </Link>
                 </Grid>
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
@@ -10,7 +9,6 @@ import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
